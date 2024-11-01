e6dfbafe80af979c8bf8874ff71b0822fa1196e1
ultron <mulerfrommarss@gmail.com>
2024-11-01 15:09:18 +0300

    FIX: paginate users list

--- controllers/authController.js
+++ controllers/authController.js
@@ -63,6 +63,7 @@ exports.getUsers = async (req, res) => {
     const page = parseInt(req.query.page, 10) || 1;
     const limit = parseInt(req.query.limit, 10) || 10;
     const search = req.query.search || "";
+    const offset = (page - 1) * limit;
 
     const options = {
       where: {},
